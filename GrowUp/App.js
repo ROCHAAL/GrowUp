f2c688161f3eba3ecb501488fe5bb7e53fd1909b
Melanie Anderson <melanieanderson1995@gmail.com>
2020-09-16 11:42:42 +0100

    added header and styled landing page

--- GrowUp/App.js
+++ GrowUp/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Header from './components/Header';
+import { v4 as uuidv4 } from 'uuid';
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+      {id: uuidv4(), text: 'Sweep'}
+    ]);
+
   return (
-    <View>
-      <Text>GrowUp!</Text>
+    <View style={styles.container}>
+      <Header />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 60,
+  },
+});
 
 export default App;
