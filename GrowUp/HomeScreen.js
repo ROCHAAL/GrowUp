c28423a95ff660e4094a531dac0d36ed17776d05
Student <student@Makerss-MacBook-Air.local>
2020-09-21 11:31:09 +0100

    HomeScreen displays 10% progress gif

--- GrowUp/HomeScreen.js
+++ GrowUp/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { View, Text, StyleSheet, ListView, FlatList, Alert, Button } from 'react-native';
+import { View, Text, StyleSheet, ListView, FlatList, Alert, Button, Image } from 'react-native';
 import { v4 as uuidv4 } from 'uuid';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -12,36 +12,6 @@ import TaskScreen from './TaskScreen';
 const HomeScreen = () => {
   const navigation = useNavigation()
 
-  const [items, setItems] = useState([
-  ]);
-
-  const deleteItem = (id) => {
-    setItems(prevItems => {
-      return prevItems.filter(item => item.id != id);
-    });
-  }
-
-  const addItem = text => {
-    if(!text) {
-      Alert.alert('Error', 'THIS HAS CHANGED', {text: 'Ok'})
-    } else {
-      setItems(prevItems => {
-        return [{id: uuidv4(), text, completed: false}, ...prevItems];
-      });
-    }
-  }
-
-  const completeItem = (id) => {
-    setItems(prevItems => {
-      return prevItems.map(item => {
-        if(id === item.id) {
-          item.completed = !item.completed
-        }
-        return item;
-      });
-    });
-  }
-
   const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -52,11 +22,7 @@ const HomeScreen = () => {
   return (
     <View style={styles.container}>
        <Header />
-       <AddItem addItem={addItem} />
-       <FlatList
-         data={items}
-         renderItem={({item}) => <ListItem completeItem={completeItem} item={item} deleteItem={deleteItem} />}
-       />
+       <Image source={require('./gifs/grow_up_10_progress.gif')} />
        <Button
         title="Go to Tasks"
         onPress={() => navigation.navigate('Task')}
